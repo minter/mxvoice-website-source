ec2640e8881d69018e723dcbac6c025e74a35fa7
minter <wade@wademinter.com>
2020-12-31 23:30:28 +0000

    Deploying to gh-pages from  @ d34952b24c0bcfbeae90c01078db930cc1e8680c 🚀

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*! main.js | Bulkit | CSS Ninja */
+
+/* ==========================================================================
+Website core JS file
+========================================================================== */
+"use strict";
+
+initPageloader();
+$(document).ready(function ($) {
+  disableSidebarLinks(); //();
+
+  initNavbar();
+  initMobileMenu();
+  highlightNavbarLinks();
+  initSidebar();
+  initPopovers();
+  initTooltips();
+  initBackgroundImages();
+  initBackToTop();
+  initGitem();
+  initScrollToHash();
+  initTestimonials();
+  initPricing();
+  initVideoEmbed();
+  initScrollReveal();
+});
